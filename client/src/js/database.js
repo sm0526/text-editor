8342e85e8d2c8246b87104fd427cacc63cd93eec
Shanon Holland <105896063+sm0526@users.noreply.github.com>
2022-12-06 20:58:12 -0700

    added in put on database.js, only a bit different than a get

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,8 +13,15 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
-
+export const putDb = async (id, content) => {
+  console.log('PUT request to update jateDB');
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readwrite');
+  const objStore = tx.objectStore('jate');
+  const req = objStore.put({ id: id, value: content })
+  const res = await req;
+  console.log('Data saved to jateDB', res);
+};
 // TODO: Add logic for a method that gets all the content from the database Need to open and show text and object store
 export const getDb = async (content) => {
   console.log('Getting data from the jateDB');
