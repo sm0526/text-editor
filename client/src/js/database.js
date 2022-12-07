ecfa272a02ae62ca1351c97a3dea852ecb0e7ccb
Shanon Holland <105896063+sm0526@users.noreply.github.com>
2022-12-06 20:52:41 -0700

    added in given starter code, checked for todos, started in database.js with the get

---
+++ client/src/js/database.js
@@ -0,0 +1,28 @@
+import { openDB } from 'idb';
+
+const initdb = async () =>
+  openDB('jate', 1, {
+    upgrade(db) {
+      if (db.objectStoreNames.contains('jate')) {
+        console.log('jate database already exists');
+        return;
+      }
+      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+      console.log('jate database created');
+    },
+  });
+
+// TODO: Add logic to a method that accepts some content and adds it to the database
+export const putDb = async (content) => console.error('putDb not implemented');
+
+// TODO: Add logic for a method that gets all the content from the database Need to open and show text and object store
+export const getDb = async (content) => {
+  console.log('Getting data from the jateDB');
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readwrite');
+  const objStore = tx.objectStore('jate');
+  const req = objStore.getAll()
+  const res = await req;
+  console.log('data saved to jateDB', res);
+}
+initdb();
